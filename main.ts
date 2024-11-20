e278a97a796f147fd9113168513c3c4b7ce33ed4
InFinity_54 <corentinkreicher@gmail.com>
2024-11-20 09:52:10 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,29 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showArrow(ArrowNames.West)
+    basic.pause(5000)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showArrow(ArrowNames.East)
+    basic.pause(5000)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
